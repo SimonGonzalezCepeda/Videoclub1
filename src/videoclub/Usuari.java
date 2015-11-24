c21f555e79beb06e13ed20c039579a9b0d1a1d78
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2015-11-24 17:10:28 +0100

    Creant funció Registrar()

--- src/videoclub/Usuari.java
+++ src/videoclub/Usuari.java
@@ -6,6 +6,10 @@
 package videoclub;
 import java.io.Serializable;
 import java.util.ArrayList;
+import gui.*;
+import static java.awt.image.ImageObserver.WIDTH;
+import javax.swing.JOptionPane;
+import videoclub.Videoclub;
 
 /**
  *
@@ -46,10 +50,34 @@ public class Usuari implements Serializable {
     /*
     *   Aquest mètode s'encarrega de registrar un Usuari.
     *
+    *   @return bolean per a comprobar que s'ha fet bé.
     */
     
-    public void Registrar(){
+    static public void Registrar(){
+        Registrarse regis = new Registrarse();
         
+        if(regis.jTextFieldUsername.getText().trim().length()==0 || regis.jPasswordField.getText().trim().length()==0){
+            JOptionPane.showMessageDialog(null, "Error:\n" + "Es obligatoria insertar al menos un usuario y una contraseña.", "Error 001", WIDTH, null);
+        }
+        
+        Usuari usuari = new Usuari();
+        
+        //int longitud=regis.jTextFieldNombre.getText().trim().length(), l2;
+        
+        usuari.setNom(regis.jTextFieldNombre.getText());
+        usuari.setCognoms(regis.jTextFieldApellidos.getText());
+        usuari.setDni(regis.jTextFieldDni.getText());
+        usuari.setCodiPostal(Integer.parseInt(regis.jTextFieldCP.getText())); //Transformen el String en int
+        usuari.setTelefon(Integer.parseInt(regis.jTextFieldTelefono.getText())); //Transformen el String en int
+        usuari.setCiutat(regis.jTextFieldCiudad.getText());
+        usuari.setDireccio(regis.jTextFieldDireccion.getText());
+        usuari.setUserName(regis.jTextFieldUsername.getText());
+        usuari.setPassword(regis.jPasswordField.getText());
+        
+        
+        videoclub.Videoclub.usuarios.add(usuari);
+
+
     }
     
     /*
